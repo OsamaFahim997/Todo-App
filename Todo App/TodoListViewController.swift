6f77a6883da8b049b70cd342ff4ebf0324655507
Osama Fahim <osamafahim@osamas-macbook-pro.local>
2019-06-18 19:21:44 +0500

    To do App is complete with datasource and delegate methods

---
+++ Todo App/TodoListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Todo App
+//
+//  Created by Osama Fahim on 18/06/2019.
+//  Copyright © 2019 Osama Fahim. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let dummyArray = ["Buy Apples", "Meet someone", "Go to World Tour"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    //MARK - TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+            cell.textLabel?.text = dummyArray[indexPath.row]
+            return cell
+    }
+
+    
+    //MARK- TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+}
+
