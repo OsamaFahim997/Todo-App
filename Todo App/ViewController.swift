198983d62c533a33f68271cb06927a6c7ae0b886
Osama Fahim <osamafahim@osamas-macbook-pro.local>
2019-06-18 18:30:08 +0500

    Initial Commit

---
+++ Todo App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo App
+//
+//  Created by Osama Fahim on 18/06/2019.
+//  Copyright © 2019 Osama Fahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
